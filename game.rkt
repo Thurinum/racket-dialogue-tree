4ea3547e77858d170f75bcea314bf8b68ae4d679
Thurinum <m.gagnon941@gmail.com>
2025-10-05 20:34:26 -0400

    refactor: Simplify main loop and leaf node case

--- game.rkt
+++ game.rkt
@@ -5,34 +5,35 @@
 (define (run-game dialogue-tree)
   (define root-node (hash-ref dialogue-tree 'root))
   (let loop ([node root-node])
-    (define choices (hash-ref node 'choices))
+    (define choices (hash-ref node 'choices '()))
       (display (hash-ref node 'text))
-      (when (eq? #t (hash-ref node 'leaf #f)) ; stop at leaf nodes
+      (when (empty? choices)
         (exit 0))
+
       (for ([choice choices]
             [i (in-naturals 1)])
         (printf "\n~a. ~a" i (hash-ref choice 'text)))
-      (define index (await-valid-response choices))
+
+      (define index (await-choice choices))
+      (when (= index -1)
+        (printf "Invalid choice, input a number between 1 and ~a" (length choices))
+        (loop node))
+
       (define choice (list-ref choices index))
       (define key (hash-ref choice 'next))
       (define next-node (hash-ref dialogue-tree (string->symbol key)))
       (loop next-node)))
 
-(define (await-valid-response choices)
-  (define num-choices (length choices))
-  (let loop ()
-    (display "\n>>> ")
-    (define input (read-line))
-    (define number (string->number (string-trim input)))
-    (define is-valid-index?
-      (and number
-        (integer? number)
-        (>= number 1)
-        (<= number num-choices)))
-    (cond
-      [is-valid-index?
-        (define index (- number 1))
-        index]
-      [else
-        (printf "Invalid choice, input a number between 1 and ~a" num-choices)
-        (loop)])))
+(define (await-choice choices)
+  (display "\n>>> ")
+  (define input (read-line))
+  (define number (string->number (string-trim input)))
+  (define is-valid-index?
+    (and number
+      (integer? number)
+      (>= number 1)
+      (<= number (length choices))))
+
+  (if is-valid-index?
+    (- number 1)
+    -1))
